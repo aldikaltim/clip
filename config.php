4215318816aa7e3702df7b2354f511b13486b7ba
aldikaltim <45003866+aldikaltim@users.noreply.github.com>
2020-07-18 03:50:22 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	
+$eksternal_version = "1.9.5.1";
+$version = "94";
+$token = "NjI4MGY2MzUtNTllYy00MmFhLWI1OGUtOWRhYjQyODVjNmNk";
+$uuid = "cc3ee931-a076-4ab4-bae1-6e9711b393cc";
+$userid = "13668499";
